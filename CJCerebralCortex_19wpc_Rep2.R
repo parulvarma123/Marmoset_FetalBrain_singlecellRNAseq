852a4c24dadbcf56cb72883fcd0cdd7f445917aa
Parul Varma <parul123@c455-014.stampede2.tacc.utexas.edu>
2022-04-21 14:28:36 -0500

    CJcerebralCortex_19wpc_Rep2_Seurat

---
+++ CJCerebralCortex_19wpc_Rep2.R
@@ -0,0 +1,70 @@
+library(Seurat)
+library(dplyr)
+library(ggplot2)
+
+#Seurat file for 19wk Marmoset fetal brain Cerebral Cortex replicate 2
+# Make Seurat object using filtered feature matrices.
+
+CJCortex19wkRep2_dir <- '~/MarmosetWork2022/CountJobs/CJCortex19wkRep2/outs/filtered_feature_bc_matrix'
+CJCortex19wkRep2.data <- Read10X(data.dir = CJCortex19wkRep2_dir)
+CJCortex19wkRep2 <- CreateSeuratObject(counts = CJCortex19wkRep2.data, project = "CJCortex19wkRep2", min.cells = 3, min.features = 200)
+CJCortex19wkRep2[["percent.mt"]] <- PercentageFeatureSet(CJCortex19wkRep2, pattern = "^MT-")
+Vlnplot1 <- VlnPlot(CJCortex19wkRep2, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+ggsave("~/MarmosetWork2022/CJCortex19wkRep2DataAnalysis/VlnplotQC1.jpeg", width = 12, height = 7, units = c("in"), dpi = 300)
+dev.off()
+
+plot1 <- FeatureScatter(CJCortex19wkRep2, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 <- FeatureScatter(CJCortex19wkRep2, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+plotcombined <- plot1 + plot2
+ggsave("~/MarmosetWork2022/CJCortex19wkRep2DataAnalysis/featureRNAplot.jpeg", width = 16, height = 5, units = c("in"), dpi = 300)
+dev.off()
+
+#based on the QC, choosing cells above 500 and less that 8000.
+CJCortex19wkRep2 <- subset(CJCortex19wkRep2, subset = nFeature_RNA > 500 & nFeature_RNA < 8000)
+
+#Normalize
+CJCortex19wkRep2 <- NormalizeData(CJCortex19wkRep2, normalization.method = "LogNormalize", scale.factor = 10000)
+#Find highly variable features
+CJCortex19wkRep2 <- FindVariableFeatures(CJCortex19wkRep2, selection.method = "vst", nfeatures = 2000)
+
+# Identify the 10 most highly variable genes.
+top10 <- head(VariableFeatures(CJCortex19wkRep2), 10)
+
+# plot variable features with and without labels.
+plot1 <- VariableFeaturePlot(CJCortex19wkRep2)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+plotcombinedvariance <- plot1 + plot2
+ggsave("~/MarmosetWork2022/CJCortex19wkRep2DataAnalysis/Varianceplot.jpeg", width = 14, height =6, units = c("in"), dpi = 300)
+dev.off()
+
+#Scaling the data. 
+all.genes <- rownames(CJCortex19wkRep2)
+CJCortex19wkRep2<- ScaleData(CJCortex19wkRep2, features = all.genes)
+
+#Perform linear dimensional reduction.
+CJCortex19wkRep2 <- RunPCA(CJCortex19wkRep2, features = VariableFeatures(object = CJCortex19wkRep2))
+
+#determine the dimensionality.
+CJCortex19wkRep2 <- JackStraw(CJCortex19wkRep2, num.replicate = 100)
+CJCortex19wkRep2 <- ScoreJackStraw(CJCortex19wkRep2, dims = 1:20)
+JackStrawPlot(CJCortex19wkRep2, dims = 1:15)
+
+#Elbow plot for finding the dimensionality.
+ElbowPlot(CJCortex19wkRep2)
+
+#Cluster the cells based on the PCs. 
+#In this case choosing 19 based on Elbow Plot.
+CJCortex19wkRep2 <- FindNeighbors(CJCortex19wkRep2, dims = 1:19)
+CJCortex19wkRep2 <- FindClusters(CJCortex19wkRep2, resolution = 0.5)
+CJCortex19wkRep2 <- RunUMAP(CJCortex19wkRep2, dims = 1:19)
+DimPlot(CJCortex19wkRep2, reduction = "umap")
+ggsave("~/MarmosetWork2022/CJCortex19wkRep2DataAnalysis/UMAP.jpeg", width = 7, height = 5, units = c("in"), dpi = 300)
+dev.off()
+
+#Save the Seurat file.
+#This can be easily loaded for cell type identification
+saveRDS(CJCortex19wkRep2, file = "~/MarmosetWork2022/ScriptsMarmosetWork2022/CJCortex19wkRep2Seur.rds")
+
+#Find the markers
+markers <- FindAllMarkers(CJCortex19wkRep2, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+write.table(markers, file ="~/MarmosetWork2022/CJCortex19wkRep2DataAnalysis/markersCJCortex19wkRep2.txt", sep = "\t", quote=F,row.names =T)
